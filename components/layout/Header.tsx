f7f35a7fb5ecde61f551870b84c4ea57f822a459
Jakub Kottnauer <jakub@0x.se>
2021-09-23 20:05:05 +0200

    Fix header item

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -3,10 +3,12 @@ import { Menu, Layout, Avatar } from "antd";
 import Link from "next/link";
 import Image from "next/image";
 import { logout, useUser } from "../../lib/api";
+import { useRouter } from "next/dist/client/router";
 
 export default function Header() {
   const { data, isLoading } = useUser();
-
+  const router = useRouter();
+  console.log(router.pathname.split("/")[1] || "home");
   return (
     <Layout.Header>
       <div
@@ -15,11 +17,16 @@ export default function Header() {
           width: "80px",
           height: "30px",
           marginTop: 10,
+          marginLeft: 30,
         }}
       >
         <Image src="/0x_logo.svg" height={30} width={30} alt="Logo" />
       </div>
-      <Menu mode="horizontal">
+      <Menu
+        mode="horizontal"
+        theme="dark"
+        selectedKeys={[router.pathname.split("/")[1] || "home"]}
+      >
         <Menu.Item key="home">
           <Link href="/">Hours</Link>
         </Menu.Item>
@@ -33,6 +40,7 @@ export default function Header() {
           <Menu.Item key="login">
             {data ? (
               <span
+                title="Logout"
                 onClick={async () => {
                   await logout();
                   location.reload();
