6a7396cf21eb4c5a190cbae8a8f4f99fa598908d
Jakub Kottnauer <jakub@0x.se>
2022-07-26 17:18:47 +0200

    Improve login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,17 @@
-import { Button } from "antd";
+import { Button, PageHeader, Typography } from "antd";
 import type { NextPage } from "next";
 
 const Login: NextPage = () => {
   return (
-    <Button onClick={() => window.location.replace("/api/oauth2")}>
-      Log in via Harvest
-    </Button>
+    <PageHeader title="Welcome 0+x-er!">
+      <Typography.Paragraph>
+        This new Harvest frontend will hopefully make tracking your worked hours
+        a breeze. To start, click the button below to log in through Harvest.
+      </Typography.Paragraph>
+      <Button onClick={() => window.location.replace("/api/oauth2")}>
+        Log in with Harvest
+      </Button>
+    </PageHeader>
   );
 };
 
