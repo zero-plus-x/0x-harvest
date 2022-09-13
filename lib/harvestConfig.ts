b96c158e0e515037c7ad15aa9f9bd17e825f3c84
Jakub Kottnauer <jakub@0x.se>
2022-09-13 12:44:23 +0200

    Update harvest url

--- lib/harvestConfig.ts
+++ lib/harvestConfig.ts
@@ -1,3 +1,3 @@
-export const HYPERONE_HARVEST_DOMAIN = "hyperone.harvestapp.com";
+export const HYPERONE_HARVEST_DOMAIN = "zeroplusx.harvestapp.com";
 export const HARVEST_ID_API_BASE_URL = "https://id.getharvest.com/api/v2";
 export const HARVEST_API_BASE_URL = "https://api.harvestapp.com/v2";
