9ae25f5b24435c182d664dd8f1661b2f50c1289f
Jakub Kottnauer <jakub@0x.se>
2023-04-21 10:35:17 +0200

    Fix urls in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,14 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { Route } from "./lib/routes";
 
 export function middleware(request: NextRequest) {
   const harvestToken = request.cookies.get("HARVEST_ACCESS_TOKEN");
 
-  if (!request.nextUrl.pathname.startsWith(Route.LOGIN) && !harvestToken) {
-    return NextResponse.redirect(new URL(Route.LOGIN, request.url));
+  if (!request.nextUrl.pathname.startsWith("/login") && !harvestToken) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 }
 
 export const config = {
-  matcher: [Route.HOME, Route.SETTINGS, Route.VACATION, Route.ADMIN],
+  matcher: ["/", "/settings", "/vacation", "/vacation", "/admin"],
 };
