d56a5e579c8056423e9689e19a239bee16ff147b
Jakub Kottnauer <jakub@0x.se>
2024-02-05 10:56:55 +0100

    Sum up even internal hours in main stats

--- components/entries/TaskHoursStatistic.tsx
+++ components/entries/TaskHoursStatistic.tsx
@@ -10,12 +10,7 @@ const TaskHoursStatistic = ({ entries }: { entries?: TimeEntry[] }) => {
     return null;
   }
   const clientHours = entries
-    ?.filter(
-      (e) =>
-        e.task.id === primaryTask?.taskId &&
-        // client task is not part of the default absence/0+x internal projects
-        !projects[e.project.id]
-    )
+    ?.filter((e) => e.task.id === primaryTask?.taskId)
     .reduce((acc, entry) => acc + entry.hours, 0);
 
   return (
